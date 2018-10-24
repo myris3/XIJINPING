a491d014ae21f64c6196b43765eab34fe57c4a29
myris3 <fritzolavmyrvang@hotmail.com>
2018-10-24 23:43:36 +0200

    Finished with greedy agent, however discovered some issues: The scorefunction in chineseCheckers should be owned by the agent in question, to allow own implementations. Also a file to instance a running game should be created, leaving the mains in the source files to run test code

--- PYTHON/game_logic/chinesecheckers.py
+++ PYTHON/game_logic/chinesecheckers.py
@@ -3,7 +3,9 @@ board_size = 13
 
 max_depth = 100
 
-aligned = False
+aligned = True
+
+max_turns = 100
 
 
 class Node():
@@ -13,10 +15,30 @@ class Node():
         self.r=0 #row placement
         self.c=0 #column placement
         self.visited = 0
+        
+#Make a deep copy of the board state
+def copyBoardTree(matrix):
+    new = newCreateBoardTree()
+    for i_row in range(0, len(matrix)):
+        for i_col in range(0, len(matrix[i_row])):
+            node = matrix[i_row][i_col]
+            new_node = new[i_row][i_col]
+            new_node.r = node.r
+            new_node.c = node.c
+            new_node.color = node.color
+    return new
 
+#Go down from 2d list of Nodes, from 2d list of strings
+def simplifyBoardRep(matrix):
+    new = newCreateBoardTree()
+    for i_row in range(0, len(matrix)):
+        for i_col in range(0, len(matrix[i_row])):
+            node = matrix[i_row][i_col]
+            new[i_row][i_col] = node.color
+    return new 
 
+#Primary way of creating the board representation
 def newCreateBoardTree():
-    #plan: separate the creating of the board, and the connecting, previous solution was way to cluttered
 
     #stage 1, create the onion, layerslayerslayers
 
@@ -80,8 +102,7 @@ def newCreateBoardTree():
 
     return matrix
 
-
-
+#Function to print the board in a nice way
 def printCheckerBoard(matrix, aligned):
     #board_size determines amount of rows
     #TODO: does not print just right for all sizes, some scaling issue 
@@ -132,25 +153,33 @@ def removeToken(row, column, matrix):
     saveToken = matrix[row][column].color 
     matrix[row][column].color = "0"
     return matrix, saveToken
-
+#Call this if pieces on board needs moved, returns moved = False if no change was made
 def moveToken(startRow, startColumn, endRow, endColumn, matrix):
+    moved = False
     if (matrix[endRow][endColumn].color == "0"):
         matrix, savedToken = removeToken(startRow, startColumn, matrix)
         matrix = placeToken(endRow, endColumn, matrix, savedToken)
         #print("Placement happened")
-    return matrix
+        moved = True
 
-def calculateScore(matrix, token, startingRow):
-    
+    return matrix, moved
+
+#Determine the relative fitness of each board state, sum of the row values for any single player. TODO: This could be more advanced
+def calculateScore(matrix, token, startingRow):    
     score = 0
     for r in range(0, len(matrix)):
         for c in range(0, len(matrix[r])):
             node = matrix[r][c]
+            #print(node)
             if node.color == token:
+                toAdd = 0
                 if startingRow == board_size-1:
-                    score += - r + board_size - 1
+                    toAdd = - r + board_size - 1
                 else:
-                    score += r
+                    toAdd = r
+                score += toAdd
+                #print("Node at ", (r, c), " evaluated at ", toAdd)
+
     return score
 
             
@@ -159,14 +188,14 @@ def calculateScore(matrix, token, startingRow):
 
 
 
-
+#Used during debugging
 def printNeighbours(node):
     neighbours = []
     for i in node.neighbours:
         neighbours.append((i.r, i.c))
     print(neighbours)
 
-
+#Debugging tool
 def possibleMovesVisual(row, column, matrix):
     #moves = possibleMoves(row, column, matrix)
     moves = {}
@@ -174,12 +203,15 @@ def possibleMovesVisual(row, column, matrix):
     for t in moves.keys():
         placeToken(t[0], t[1], matrix,  "x")
 
+#after looking for possible moves, clear all visited flags in each node
+#used in fullRecursivePossibleMoves
 def clearVisited(matrix):
     for row in matrix:
         for node in row:
             node.visited = 0
     return matrix
 
+#Primary function to find all moves any piece can do on the board
 def fullRecursivePossibleMoves(depth, row, column, matrix, moves, origin):
     # This seems to actually work, i dub the the delta rule
 
@@ -213,7 +245,7 @@ def fullRecursivePossibleMoves(depth, row, column, matrix, moves, origin):
             
 
 
-    
+#Create default starting positions for player 1 and player 2   
 def createStartingPositions(token1, token2):
     matrix = newCreateBoardTree()
     #Assuming board size is reasonable
@@ -231,11 +263,9 @@ def createStartingPositions(token1, token2):
 
     return matrix
 
+#Test to see the starting position of any default game
 def run():
     matrix = createStartingPositions()
-    
-    #second argument in printCheckerBoard determines output format, aligned or not aligned 
-   
     printCheckerBoard(matrix, False)
     print("scores\tplayer1: ",calculateScore(matrix, "1", 0),"\n\tplayer2: ",calculateScore(matrix,"2",len(matrix)-1))
 
--- PYTHON/game_logic/greedy_agent.py
+++ PYTHON/game_logic/greedy_agent.py
@@ -1,12 +1,23 @@
 import chinesecheckers as helper
 import time
 
+
+
+#Agent class, a way to contain useful data and methods pertaining any player
 class Greedy_Agent():
     def __init__(self, token, startingRow):
         self.startingRow = startingRow
         self.token = token
-        
-
+    #Helper method, used in determining the score of a board position relative to the agent 
+    def calculateNodeScore(self, node):
+        score = 0
+        if self.token == "1":
+            score = node.r
+        else:
+            score = helper.board_size-1 - node.r
+        return score
+    #Primary method, does a lot of things, in summary, returns two tuples, represents origin game piece and destination position on the gameboard.
+    #Finally, it returns a score, the score being the value of the current  board state after the move has been performed
     def suggestedMove(self, matrix):
         
         #plan: 1.enumerate all moves
@@ -15,7 +26,7 @@ class Greedy_Agent():
         #      4. do some default cases, return some special value if no change is made to board state
         
         
-        
+    
         listOfMoveDicts = []
 
         for row in matrix:
@@ -31,34 +42,113 @@ class Greedy_Agent():
                             matrix,
                             moves,
                             node)
+                    if len(moves.keys()) == 0:
+                        continue
+                    #print("Printing contents of moves: ", moves.keys())
                     listOfMoveDicts.append(moves)
         
         highest_delta = 0
-        suggestedMove = (-1, -1)
-        origin = (-1, -1)
+        suggestedMove = (0, 0)
+        suggested_origin = (0, 0)
+        highest_score = 0        
         for moves in listOfMoveDicts:
             for move in moves.keys():
 
-                temp = matrix
-                scoreBefore = helper.calculateScore(temp, self.token, self.startingRow)
+                #moves is a dict, keys are tuples with the move to be considered, the data with the key is a list, with the origin node at element 0
+                temp = helper.copyBoardTree(matrix) 
 
-                temp = helper.moveToken(move[0], move[1], moves[move][0].r, moves[move][0].c, temp)
+                #renaming for readability
+                destination = (move[0], move[1])
+                origin = (moves[move][0].r, moves[move][0].c)
+
+                scoreBefore = helper.calculateScore(temp, self.token, self.startingRow)
+                #print(move)
+                temp, dump = helper.moveToken(origin[0], origin[1], destination[0], destination[1], temp)
+                
                 scoreAfter = helper.calculateScore(temp, self.token, self.startingRow)
                 
-                delta = scoreAfter #- scoreBefore
-                print("delta is", delta)
+                delta = scoreAfter - scoreBefore
+                #print("delta is", delta)
 
-                moves[move][1] = delta
-            
+                #moves[move][1] = delta
+                 
                 if delta > highest_delta:
+                    #print("Score before is ",scoreBefore, ". Score after is ", scoreAfter, "move ",origin, destination, " is considered")
                     highest_delta = delta
-                    suggestedMove = move
-                    origin = moves[move][0]
+                    suggestedMove = destination
+                    suggested_origin = origin
+                    highest_score = scoreAfter
 
-            if (suggestedMove == (-1, -1)):
-                #print("No moves to make for player ", self.token)
-                return (0,0), (0,0)
-        return suggestedMove, (origin.r, origin.c)
+        # trigger this if there seems to be no good greedy option, find the lowest score node, and determine direction to highest score increase
+        if highest_delta == 0: # find the piece with lowest score, plot a path for it to closest increase in score
+            
+            candidate = (0, 0)
+            scores = {}
+            #Iterate over each node, getting the scores for each, put them in a dict, with similar scores on same key in dict
+            for row in matrix:
+                for node in row:
+                    if self.token == node.color:
+                        score = self.calculateNodeScore(node)
+                        if score in scores:
+                            scores[score].append(node)
+                        else:
+                            scores[score] = [node]
+                            
+            #Go from lowest score to "infinity", stopping at the lowest score it finds, and evaluates
+            for i in range(0, 1000):
+                if i in scores:
+                    spaces_and_nodes = {}
+
+                    for node in scores[i]:
+                        #evaluate free space on previous and next row
+                        #pick the node with closest distance to the free space wich will increase its score
+                        rowToExamine = -1
+
+                        #See if score gradient points up a row, or down a row
+                        if self.calculateNodeScore(matrix[node.r-1][0]) > i:
+                            rowToExamine = node.r-1
+                            #print("Considering up, i is  ",i)
+                        else:
+                            rowToExamine = node.r+1
+                            #print("Considering down, i is ", i)
+                        free_spaces = []
+                        
+                        #Consider each free space on the row in question, and calculate the distance to it
+                        for n in matrix[rowToExamine]:
+                            if n.color == "0":
+                                #append a list with the node, and the distance, if distance is negative, space is to the right of node, else to the left
+                                free_spaces.append([n, node.c - n.c])
+
+                        spaces_and_nodes[node] = free_spaces
+                    
+                    signed_distance = 0
+                    shortest_distance = 10000
+                    destination = (0, 0)
+                    origin = (0, 0)
+                    #Try to find a node with shortest distance to score increase
+                    for node in spaces_and_nodes.keys():
+                        for item in spaces_and_nodes[node]:
+                           # print("At ", node.color, item)
+                            if abs(item[1]) <shortest_distance:
+                                shortest_distance = abs(item[1])
+                                signed_distance = item[1]
+                                destination = (item[0].r, item[0].c)
+                                origin = (node.r, node.c)
+                                #print("considering ", origin, destination)
+                    #Heres a rule to determine to go left or right on the row (the idea is to break out of the local optimum)
+                    if signed_distance<0: 
+                        suggestedMove = (origin[0], origin[1]+1)
+                    else:
+                        suggestedMove = (origin[0], origin[1]-1)
+                    
+                    suggested_origin = origin
+
+                    break
+                    
+
+
+
+        return suggestedMove, suggested_origin, highest_score
 
     
             
@@ -69,48 +159,86 @@ class Game():
         self.gameBoard = board
         self.gameNotOver = True
         self.turn = 1
-        self.winner = "3"
-        self.maxTurns = 5
+        self.current = "3"
+        self.maxTurns = helper.max_turns
 
-    
+    #main loop to drive a game between two agents 
     def play(self):
-        print("Initial Board ")
+        print("|___________________________________|")
+        print("|___________Initial Board___________|")
+        print("|___________________________________|")
         helper.printCheckerBoard(self.gameBoard, helper.aligned)
         print("")
+
+
+        winner = ""
+        loser = ""
+        score1 = 0
+        score2 = 0
+        turns = 0
+        #shape of this sould be a copy of the board, turn count, scores of each player
+        turnStack = []
 
         while self.gameNotOver:
             move = (0, 0)
             toMove = (0, 0)
-
+            #score = 0
+            player = self.player1
+            moved = True
+            
+            #Alternate between player1 and 2
             if self.turn%2 != 0:
-                move, toMove = self.player1.suggestedMove(self.gameBoard)
-                self.winner = self.player1.token
-
+                move, toMove, tempscore1 = self.player1.suggestedMove(self.gameBoard)
+                self.current = self.player1.token
+                winner = self.player1.token
+                loser = self.player2.token
+                if tempscore1>score1:
+                    score1=tempscore1
             else:
-                move, toMove = self.player2.suggestedMove(self.gameBoard)
-                self.winner= self.player2.token
+                move, toMove, tempscore2 = self.player2.suggestedMove(self.gameBoard)
+                self.current= self.player2.token
+                winner = self.player2.token
+                loser = self.player1.token
+                if tempscore2>score2:
+                    score2=tempscore2
 
-            if move == toMove or self.turn>self.maxTurns:
-                print("Gameover is set on turn "+str(self.turn))
-                self.gameNotOver = False
-                #break
-
-            self.gameBoard = helper.moveToken(
+                    
+            #boardCopy = helper.copyBoardTree(self.gameBoard)
+            self.gameBoard, moved = helper.moveToken(
                     toMove[0],
                     toMove[1],
                     move[0],
                     move[1],
                     self.gameBoard)
+            #maybe checking move==toMove is redundant 
+            if self.turn>=self.maxTurns or not moved:
+                print("Game ended on turn "+str(self.turn-2))
+                turns = self.turn-2
+                self.gameNotOver = False
+                    
+                turnStack.pop(len(turnStack)-1)
+                
+                break
 
-            helper.printCheckerBoard(self.gameBoard, helper.aligned)
-            print(self.winner, " moved ", toMove, move)
-            print("scores\tplayer1: ",helper.calculateScore(self.gameBoard, self.player1.token, 0), "\n\tplayer2: ",helper.calculateScore(self.gameBoard, self.player2.token, len(self.gameBoard)-1))
+            #else:
+                #print("Turn ", self.turn,". Player ", self.current, " moved ", toMove, move)
+            #time.sleep(5)
+            #helper.printCheckerBoard(self.gameBoard, helper.aligned)
+           # print("scores\tplayer1: ",helper.calculateScore(self.gameBoard, self.player1.token, 0), "\n\tplayer2: ",helper.calculateScore(self.gameBoard, self.player2.token, len(self.gameBoard)-1))
 
-            print("")
+            #print("")
+            if self.gameNotOver: #This goes into ml algorithm, bookkeeping
+                boardCopy = helper.simplifyBoardRep(self.gameBoard)
+                #self.turn
+                player1_score = helper.calculateScore(self.gameBoard, self.player1.token, self.player1.startingRow)
+                player2_score = helper.calculateScore(self.gameBoard, self.player2.token, self.player2.startingRow)
+                turnStack.append([boardCopy, self.turn, player1_score, player2_score])
             
             self.turn +=1
-
+        helper.printCheckerBoard(self.gameBoard, helper.aligned) 
         print("Game has ended")
+
+        return winner, score1, turns, loser, score2, turnStack
 
 
 
@@ -123,7 +251,9 @@ def run():
     agent1 = Greedy_Agent(token1, 0)
     agent2 = Greedy_Agent(token2, len(board)-1)
     game = Game(agent1, agent2, board)
-    game.play()
+    results = game.play()
 
+    print("Winner is player " + str(results[0]) + " with " + str(results[1]) + " points on turn " + str(results[2]) + ".\nLoser is player "+ str(results[3])+ " with "+str(results[4])+" points." )
+    #print(results[5])
 if __name__ == "__main__":
     run()
